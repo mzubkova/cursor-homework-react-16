e7e77351360d9c11f68fd1b52eb1916590235a45
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-09-11 18:02:30 +0300

    homework 16

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Contacts from "./contacts/contacts";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Contacts />
+      </div>
+    );
+  }
+}
+
+export default App;
